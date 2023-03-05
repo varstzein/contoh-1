c39d6890b9a343e3be0716fb9548423bc7397884
MUHAMMAD VITO ARISTAWIDYA <vitoarista861@gmail.com>
2023-03-05 19:19:31 +0700

    sudah modifikasi

---
+++ rumus 2.py
@@ -0,0 +1,6 @@
+def luas_persegi() :
+    sisi1 = int(input("masukkan sisi pertama : "))
+    sisi2 = int(input("masukkan sisi kedua : "))
+    luas = sisi1*sisi2
+    print("Luas persegi adalah =", luas)
+luas_persegi()
